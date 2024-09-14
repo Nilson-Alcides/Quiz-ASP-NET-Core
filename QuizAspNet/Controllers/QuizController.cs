1d0fcdbb45f5dd069b5fcf74fc819009cb16fc55
Nilson-Alcides <nilson_alcides@hotmail.com>
2024-09-14 13:11:10 -0300

    Controller Quiz Index cria as alternativas

---
+++ QuizAspNet/Controllers/QuizController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuizAspNet.Models;
+
+namespace QuizAspNet.Controllers
+{
+    public class QuizController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Criando um exemplo de pessoa
+            var pessoa = new Pessoa
+            {
+                Id = 1,
+                Nome = "João",
+                Idade = 25,
+                Quizzes = new List<Quiz>
+                {
+                    new Quiz
+                    {
+                        Id = 1,
+                        Titulo = "Quiz de Conhecimentos Gerais",
+                        Questoes = new List<Acervo>
+                        {
+                            new Acervo
+                            {
+                                Id = 1,
+                                Pergunta = "Qual é a capital da França?",
+                                RespostaCorreta = "Paris",
+                                Alternativas = new List<string> { "Paris", "Londres", "Berlim", "Madri" }
+                            },
+                            new Acervo
+                            {
+                                Id = 2,
+                                Pergunta = "Qual é o maior oceano do mundo?",
+                                RespostaCorreta = "Oceano Pacífico",
+                                Alternativas = new List<string> { "Oceano Atlântico", "Oceano Pacífico", "Oceano Índico", "Oceano Ártico" }
+                            },
+                            new Acervo
+                            {
+                                Id = 2,
+                                Pergunta = "Qual brasil esta em qual continente?",
+                                RespostaCorreta = "Continente americano",
+                                Alternativas = new List<string> { "Continente americano", "Continente asiático", "Continente europeu", "Continente africano" }
+                            }
+                        }
+                    }
+                }
+            };
+
+            ViewBag.Pessoa = pessoa;
+            ViewBag.Quiz = pessoa.Quizzes[0];
+
+            return View();
+        }
+
+       
+    }
+}
+
+
--- QuizAspNet/obj/Debug/net8.0/QuizAspNet.AssemblyInfo.cs
+++ QuizAspNet/obj/Debug/net8.0/QuizAspNet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizAspNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad53a2e3cfbbd5e1670a68ed90535de4208ef6a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f89c336904649abddb21e657bcf0a4735c142fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizAspNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizAspNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
