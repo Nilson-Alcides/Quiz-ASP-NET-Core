a92c3837bfe4cd969cf73727085d251c5d52262c
Nilson-Alcides <nilson_alcides@hotmail.com>
2024-09-14 13:41:33 -0300

    Adicionando QuizEsporteController

---
+++ QuizAspNet/Controllers/QuizEsportesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuizAspNet.Controllers
+{
+    public class QuizEsporteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- QuizAspNet/obj/Debug/net8.0/QuizAspNet.AssemblyInfo.cs
+++ QuizAspNet/obj/Debug/net8.0/QuizAspNet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizAspNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37dc72f3a384acd99a40d9b0f6b49ccb4f6d873f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71279ec516a4b944dea66b28ca1210c845b2d4b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizAspNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizAspNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
