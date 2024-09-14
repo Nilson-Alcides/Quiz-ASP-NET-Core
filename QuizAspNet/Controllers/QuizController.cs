09f120e81c11daec81d66f223579a2485bb24c01
Nilson-Alcides <nilson_alcides@hotmail.com>
2024-09-14 13:12:25 -0300

    QuizController Coleta o resultado e calcula o acerto

--- QuizAspNet/Controllers/QuizController.cs
+++ QuizAspNet/Controllers/QuizController.cs
@@ -53,7 +53,72 @@ namespace QuizAspNet.Controllers
             return View();
         }
 
-       
+        [HttpPost]
+        public IActionResult Resultado(List<string> respostas)
+        {
+            // Obtendo o quiz e as questões
+            var pessoa = new Pessoa
+            {
+                Id = 1,
+                Nome = "João",
+                Idade = 25,
+                Quizzes = new List<Quiz>
+                {
+                    new Quiz
+                    {
+                        Id = 1,
+                        Titulo = "Quiz de Conhecimentos Gerais",
+                        Questoes = new List<Acervo>
+                        {
+                            new Acervo
+                            {
+                                Id = 1,
+                                Pergunta = "Qual é a capital da França?",
+                                RespostaCorreta = "Paris",
+                                Alternativas = new List<string> { "Paris", "Londres", "Berlim", "Madri" }
+                            },
+                            new Acervo
+                            {
+                                Id = 2,
+                                Pergunta = "Qual é o maior oceano do mundo?",
+                                RespostaCorreta = "Oceano Pacífico",
+                                Alternativas = new List<string> { "Oceano Atlântico", "Oceano Pacífico", "Oceano Índico", "Oceano Ártico" }
+                            },
+                            new Acervo
+                            {
+                                Id = 2,
+                                Pergunta = "Qual brasil esta em qual continente?",
+                                RespostaCorreta = "Continente americano",
+                                Alternativas = new List<string> { "Continente americano", "Continente asiático", "Continente europeu", "Continente africano" }
+                            }
+                        }
+                    }
+                }
+            };
+
+            var quiz = pessoa.Quizzes[0];
+            var questoes = quiz.Questoes;
+
+            // Calculando o número de respostas corretas
+            int corretas = 0;
+
+            for (int i = 0; i < questoes.Count; i++)
+            {
+                if (respostas[i] == questoes[i].RespostaCorreta)
+                {
+                    corretas++;
+                }
+            }
+
+            // Passando informações para a View usando ViewBag
+            ViewBag.Nome = pessoa.Nome;
+            ViewBag.Corretas = corretas;
+            ViewBag.Total = questoes.Count;
+            ViewBag.Respostas = respostas;
+            ViewBag.Quiz = quiz;
+
+            return View();
+        }
     }
 }
 
