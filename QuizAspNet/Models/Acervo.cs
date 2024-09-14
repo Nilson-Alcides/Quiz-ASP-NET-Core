72fea3c3aa926051c7e94072e0c6772691ae4ca9
Nilson-Alcides <nilson_alcides@hotmail.com>
2024-09-14 12:57:00 -0300

    Adicionando classe Acervo

---
+++ QuizAspNet/Models/Acervo.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjetoGilsonDaniel_RollSP.Models
+{
+    public class Acervo
+    {
+        public int Id { get; set; }
+        public string Pergunta { get; set; }
+        public string RespostaCorreta { get; set; }
+        public List<string> Alternativas { get; set; }  
+    }
+}
--- QuizAspNet/obj/Debug/net8.0/QuizAspNet.AssemblyInfo.cs
+++ QuizAspNet/obj/Debug/net8.0/QuizAspNet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizAspNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7a683fce44d620a967a40b502167fd7a8f6e7ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a674723274ed2b1066a6e5890043ff0ccec80d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizAspNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizAspNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
